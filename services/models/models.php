e976c0e946cf53fc1eaa841893a82a72a9d9c05b
hosein <85564616+hoseinalizadehkhiabani@users.noreply.github.com>
2022-05-30 12:13:17 +0430

    6

--- services/models/models.php
+++ services/models/models.php
@@ -1,12 +1,9 @@
 <?php
 
-// require (__DIR__."/BaseModel.php");
 
 function load_models($filename){
     try{
-        // if(class_exists($filename))
-        // return;
-
+      
         $filePath =__DIR__."/$filename.php";
         if(file_exists($filePath))
         {
